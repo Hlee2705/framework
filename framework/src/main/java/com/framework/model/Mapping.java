ce3cde2f8447522cbbf92a15e2613c5a2c287ea4
Hlee2705 <randrianasolonomenavalisoa@gmail.com>
2026-06-22 23:13:05 +0300

    création de la classe Mapping permet de lier une classe et une méthode spécifique pour une url donnée

---
+++ framework/src/main/java/com/framework/model/Mapping.java
@@ -0,0 +1,23 @@
+package com.framework.model;
+
+import java.lang.reflect.Method;
+
+// permet de lier une classe et une méthode spécifique pour une url donnée
+public class Mapping {
+    private Class<?> controllerClass;
+    private Method method;
+
+    public Mapping(Class<?> controllerClass, Method method) {
+        this.controllerClass = controllerClass;
+        this.method = method;
+    }
+
+    public Class<?> getControllerClass() {
+        return controllerClass;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+}
