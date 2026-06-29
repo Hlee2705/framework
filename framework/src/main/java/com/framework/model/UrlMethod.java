39ad63963eefad9d02accf69f076d50bdfc8926e
Hlee2705 <randrianasolonomenavalisoa@gmail.com>
2026-06-29 22:02:23 +0300

    création de la classe UrlMethod

---
+++ framework/src/main/java/com/framework/model/UrlMethod.java
@@ -0,0 +1,28 @@
+package com.framework.model;
+
+public class UrlMethod {
+    private String url;
+    private String method;
+
+    public UrlMethod(){}
+
+    public UrlMethod(String url, String method){
+        this.url = url:
+        this.method = method;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getMethod() {
+        return method;
+    }
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    
+}
