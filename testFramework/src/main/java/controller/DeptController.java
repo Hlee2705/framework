7c3aa9a76fd5eb04e8d565401e602493dcdad69f
Hlee2705 <randrianasolonomenavalisoa@gmail.com>
2026-07-13 22:58:15 +0300

    update deptController

--- testFramework/src/main/java/controller/DeptController.java
+++ testFramework/src/main/java/controller/DeptController.java
@@ -3,33 +3,60 @@ package controller;
 import org.springframework.stereotype.Controller;
 
 import com.framework.annotation.UrlMapping;
+import com.framework.model.ModelView;
 
 @Controller
 public class DeptController {
 
     @UrlMapping(value = "dept/new", method = "GET")
-    public void create() {
-        System.out.println("Méthode create() exécutée !");
+    public ModelView create() {
+
+        ModelView mv = new ModelView("dept-form");
+
+        mv.addObject("titre", "Création d'un département");
+
+        return mv;
     }
 
     @UrlMapping(value = "dept/new", method = "POST")
-    public void save() {
-        System.out.println("Méthode save() exécutée !");
+    public ModelView save() {
+
+        ModelView mv = new ModelView("dept-save");
+
+        mv.addObject("message", "Département enregistré avec succès");
+
+        return mv;
     }
 
     @UrlMapping(value = "dept/list", method = "GET")
-    public void list() {
-        System.out.println("Méthode list() exécutée !");
+    public ModelView list() {
+
+        ModelView mv = new ModelView("dept-list");
+
+        mv.addObject("nom", "Informatique");
+        mv.addObject("chef", "Rakoto");
+        mv.addObject("effectif", 120);
+
+        return mv;
     }
 
     @UrlMapping(value = "andrana")
-    public void andrana() {
-        System.out.println("andrana fotsiny");
+    public ModelView andrana() {
+
+        ModelView mv = new ModelView("test");
+
+        mv.addObject("texte", "Essai du framework");
+
+        return mv;
     }
 
     @UrlMapping(value = "pomme", method = "GET")
+    public ModelView pomme() {
 
-    public void pomme() {
-        System.out.println("j'adore les pommes");
+        ModelView mv = new ModelView("pomme");
+
+        mv.addObject("fruit", "J'adore les pommes");
+
+        return mv;
     }
 }
